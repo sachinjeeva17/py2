69b60d9c38c912efed3c95471aa43d40a5781845
sachinjeeva17 <52067230+sachinjeeva17@users.noreply.github.com>
2019-07-03 15:05:30 +0530

    Update j4.py

--- j4.py
+++ j4.py
@@ -1,6 +1,6 @@
-ch = input("Enter a character:")
+ch = input()
 if((ch>='a' and ch<= 'z') or (ch>='A' and ch<='Z')):
-    print(ch, " Alphabet")
+    print(" Alphabet")
 else:
-    print(ch, "No")
+    print("No")
   
